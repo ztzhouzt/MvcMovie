0b383f5683782fb26c5824d5ce52b9ed52730c0d
zhou <1249126625@qq.com>
2019-02-15 15:03:05 +0800

    ffff

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -30,7 +30,7 @@ namespace MvcMovie
                     SeedData.Initialize(services);
                 }
                 catch (Exception ex)
-                {
+                { 
                     var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occurred seeding the DB.");
                 }
