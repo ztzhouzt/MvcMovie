dad8d7038ab8a0c01a96aba751c2516bf807f961
zhou <1249126625@qq.com>
2019-02-15 14:53:03 +0800

    添加项目文件。

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome()
+        {
+            return "------this is Welcome--------";
+        }
+        public IActionResult Other(string Name,int NumTimes=1)
+        {
+            ViewData["Message"] = "Hello " + Name;
+            ViewData["NumTimes"] = NumTimes;
+            //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            return View();
+        }
+    }
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        
+        /// <summary>
+        /// 发布日期
+        /// </summary>
+        [Display(Name ="Release Data")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        /// <summary>
+        /// 电影类型
+        /// </summary>
+        public string Genre { get; set; }
+
+        /// <summary>
+        /// 价格
+        /// </summary>
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Price { get; set; }
+
+    }
+}
